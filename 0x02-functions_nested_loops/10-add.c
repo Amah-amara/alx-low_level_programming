d99de140b9818db21918116e753cda3ec554902d
Amah-amara <cynama99@gmail.com>
2022-12-14 00:43:51 +0100

    task10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - entry point for addition
+ * @a: one value to be added
+ * @b: second value to be added
+ * Return: 0
+ */
+
+int add(int a, int b)
+
+{
+	return (a + b);
+}
