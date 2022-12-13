ef164ced7cb83007cd9d6c4c0f697420f49f3b30
Amah-amara <cynama99@gmail.com>
2022-12-13 22:17:53 +0100

    tas43

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _islower checks if c is a letter and returns 1
+ * @c: unsigned int value to be compared with ASCII value
+ * 
+ * Return: 1 if alphabet otherwise 0
+ */
+int _isalpha(int c)
+{
+	if (c >= 65 && c <= 122)
+		return (1);
+	else
+		return (0);
+}
