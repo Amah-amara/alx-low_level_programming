2943e6c9cc6e27423acfc3cafb00ea2163d122b9
Amah-amara <cynama99@gmail.com>
2023-01-06 22:22:00 +0100

    static lib

--- 0x09-static_libraries/2-strncpy.c
+++ 0x09-static_libraries/2-strncpy.c
@@ -10,8 +10,10 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 	int j;
+
 	j = 0;
-	
+
+
 	while (j < n && src[j] != '\0')
 	{
 		dest[j] = src[j];
@@ -22,6 +24,5 @@ char *_strncpy(char *dest, char *src, int n)
 		dest[j] = '\0';
 		j++;
 	}
-	
 	return (dest);
 }
