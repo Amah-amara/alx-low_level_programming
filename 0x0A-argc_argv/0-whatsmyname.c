49890b4cb4307306f0dc0c2581b8cf2fd725c7a8
Amah-amara <cynama99@gmail.com>
2023-01-07 18:59:44 +0100

    argc task

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - function that prints its name 
+ * @argc: count arguments
+ * @argv: arguments
+ * Return: 0
+ *
+ */
+int main(int argc, char **argv)
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - prints the arguments it receives
+ * @argc: count arguments
+ * @argv: the arguments
+ * Return: 0
+ *
+ */
+int main(int argc, char **argv)
+{
+	int count = 0;
+
+	if (argc > 0)
+	{
+		while (count < argc)
+		{
+			printf("s\n"' argv[count]);
+			count++;
+		}
+	}
+	return (0);
+}
