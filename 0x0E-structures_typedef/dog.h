193dd3867d0417f2bfdfc27a16878e799f1a59c1
Amah-amara <cynama99@gmail.com>
2023-01-16 22:21:01 +0100

    task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - user defined data type
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owners name
+ */
+
+struct dog
+{
+	char *name
+	float age
+	char *owner
+};
+#endif
