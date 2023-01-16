36daa4596dbe16aab2a8adcddac93004a78222ac
Amah-amara <cynama99@gmail.com>
2023-01-16 15:46:44 +0100

    Task 3

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,3 +1,4 @@
 #ifndef _FUNCTION_LIKE_MACRO_
-#define _FUNCTION_LIKE_MACRO_ ABS(X) ((X < (0)) ? ((X) * (-1)) : (X))
+#define _FUNCTION_LIKE_MACRO_ 
+#define ABS(X) ((X < (0)) ? ((X) * (-1)) : (X))
 #endif
