27326ef3a47fc17b9dabfb3978114ad13dc355b6
Amah-amara <cynama99@gmail.com>
2023-01-16 23:43:52 +0100

    task 5

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * new_dog -new dog
+ * @name: name's dog
+ * @age: age's dog
+ * @owner: owner's dog
+ * Return: newdog
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+
+}
