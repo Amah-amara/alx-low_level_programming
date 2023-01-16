ec4404aa4bd526f0ad1db65df8010cd84b148bfd
Amah-amara <cynama99@gmail.com>
2023-01-16 15:13:34 +0100

    Task 2

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - program compilation starts here
+ * description: the program will print the name of the file as a string literal
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("filename is : %s\n", __FILE__);
+	return (0);
+}
