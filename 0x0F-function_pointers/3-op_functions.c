86a296feb1271a24a5ba9b1b99cff0834b668d0f
Amah-amara <cynama99@gmail.com>
2023-01-19 18:11:12 +0100

    task hmm

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -13,9 +13,10 @@ int op_add(int a, int b)
 }
 
 /**
- * op_sub - calculates subtraction of two int
+ * op_sub - calculates subtraction of two integers
  * @a: first integer
  * @b: second integer
+ * Return: subtraction of a and b
  */
 int op_sub(int a, int b)
 {
@@ -23,9 +24,10 @@ int op_sub(int a, int b)
 }
 
 /**
- * op_mul - calculates mproduct of two integers
+ * op_mul - calculates product of two integers
  * @a: param1
  * @b: param2
+ * Return: product of two integers
  */
 int op_mul(int a, int b)
 {
@@ -33,9 +35,10 @@ int op_mul(int a, int b)
 }
 
 /**
- * op_div: calculates division of two ints
+ * op_div - calculates division of two integers
  * @a: param1
  * @b: param2
+ * Return: division of a and b
  */
 int op_div(int a, int b)
 {
@@ -43,9 +46,10 @@ int op_div(int a, int b)
 }
 
 /**
- * op_mod - calculates the remainder of division of two ints
+ * op_mod - calculates the remainder of division of two integers
  * @a: param1
  * @b: param2
+ * Return: remainder of division of a and b
  */
 int op_mod(int a, int b)
 {
