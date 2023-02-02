d0288181e44a59c05a7dd6a05607ad479e68a4b8
Amah-amara <cynama99@gmail.com>
2023-02-02 15:21:36 +0100

    task 2

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -3,7 +3,7 @@
 /**
  * get_bit - returns the value of a bit at a given index
  * @n: unsigned long int to search
- * 2index: index to return
+ * index: index to return
  * Return: the value of the bit at index or -1 if error
  */
 int get_bit(unsigned long int n, unsigned int index)
